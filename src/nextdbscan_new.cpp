21379ce01f65d5ea35cac7e302b59a384786378e
Ernir Erlingsson <ernire@gmail.com>
2019-11-21 23:52:28 +0100

    Wrap for the day

--- src/nextdbscan_new.cpp
+++ src/nextdbscan_new.cpp
@@ -49,6 +49,12 @@ SOFTWARE.
 
 namespace nextdbscan {
 
+#ifdef CUDA_ON
+    // V100
+    static const int CUDA_BLOCKS = 128;
+    static const int CUDA_THREADS = 1024;
+#endif
+
     static const int UNASSIGNED = -1;
 
     static const uint8_t NC = 0;
@@ -590,13 +596,10 @@ namespace nextdbscan {
             thrust::device_vector<float> &v_max_cell_dim,
             thrust::device_vector<float> &v_last_max_cell_dim,
             const uint l, const uint max_d) {
-
         v_min_cell_dim.resize(v_device_cell_begin.size() * max_d, 0);
         v_max_cell_dim.resize(v_min_cell_dim.size(), 0);
-
         float* v_min_input_ptr;
         float* v_max_input_ptr;
-
         if (l == 0) {
             v_min_input_ptr = thrust::raw_pointer_cast(&v_coords[0]);
             v_max_input_ptr = thrust::raw_pointer_cast(&v_coords[0]);
@@ -604,7 +607,7 @@ namespace nextdbscan {
             v_min_input_ptr = thrust::raw_pointer_cast(&v_last_min_cell_dim[0]);
             v_max_input_ptr = thrust::raw_pointer_cast(&v_last_max_cell_dim[0]);
         }
-        determine_min_max<<<128, 1024>>>(
+        determine_min_max<<<CUDA_BLOCKS, CUDA_THREADS>>>(
             thrust::raw_pointer_cast(&v_device_index_map[0]),
             thrust::raw_pointer_cast(&v_device_cell_begin[0]),
             thrust::raw_pointer_cast(&v_device_cell_ns[0]),
@@ -615,8 +618,6 @@ namespace nextdbscan {
             v_device_cell_begin.size(),
             max_d,
             l);
-
-        // TODO interchange current and last instead of copying
         v_last_min_cell_dim.resize(v_min_cell_dim.size());
         thrust::copy(v_min_cell_dim.begin(), v_min_cell_dim.end(), v_last_min_cell_dim.begin());
         v_last_max_cell_dim.resize(v_max_cell_dim.size());
@@ -1106,8 +1107,8 @@ namespace nextdbscan {
             thrust::device_vector<uint> &v_unique_cnt,
             thrust::device_vector<uint> &v_indexes,
             thrust::device_vector<ull> &v_dims_mult,
+            thrust::device_vector<uint> &v_tmp,
             const uint size, const uint l, const uint max_d) {
-
         if (l == 0) {
             v_value_map.resize(size);
             v_unique_cnt.resize(size);
@@ -1119,8 +1120,7 @@ namespace nextdbscan {
         v_device_index_map.resize(size);
         thrust::sequence(v_device_index_map.begin(), v_device_index_map.end());
         thrust::fill(v_unique_cnt.begin(), v_unique_cnt.end(), 0);
-        // V100
-        index_kernel<<<128 ,1024>>>(
+        index_kernel<<<CUDA_BLOCKS ,CUDA_THREADS>>>(
             thrust::raw_pointer_cast(&v_coords[0]),
             thrust::raw_pointer_cast(&v_coord_indexes[0]),
             thrust::raw_pointer_cast(&v_value_map[0]),
@@ -1131,15 +1131,14 @@ namespace nextdbscan {
             v_level_eps[l]
         );
         thrust::sort_by_key(v_value_map.begin(), v_value_map.begin() + size, v_device_index_map.begin());
-        thrust::device_vector<uint> v_tmp(size);
+        v_tmp.resize(size);
         auto ptr_indexes = thrust::raw_pointer_cast(&v_coord_indexes[0]);
         thrust::transform(thrust::device,
                 v_device_index_map.begin(),
                 v_device_index_map.end(),
                 v_tmp.begin(),
                 [=] __device__ (const auto val) { return ptr_indexes[val]; });
-        // V100
-        count_unique_groups<<<128,1024>>>(
+        count_unique_groups<<<CUDA_BLOCKS,CUDA_THREADS>>>(
                 thrust::raw_pointer_cast(&v_value_map[0]),
                 thrust::raw_pointer_cast(&v_unique_cnt[0]),
                 size);
@@ -1409,11 +1408,13 @@ namespace nextdbscan {
         thrust::device_vector<float> v_last_min_cell_dim;
         thrust::device_vector<float> v_max_cell_dim;
         thrust::device_vector<float> v_last_max_cell_dim;
+        thrust::device_vector<uint> v_tmp;
         for (int l = 0; l < max_levels; ++l) {
             size = cu_index_level_and_get_cells(v_device_coords, v_device_index_map,
                     v_device_cell_ns, v_device_cell_begin, v_device_min_bounds,
                     v_device_dims_mult, v_device_eps_levels, v_device_value_map,
-                    v_coord_indexes, v_unique_cnt, v_indexes, v_device_dims_mult, size, l, max_d);
+                    v_coord_indexes, v_unique_cnt, v_indexes, v_device_dims_mult, v_tmp,
+                    size, l, max_d);
             cu_calculate_level_cell_bounds(v_device_coords, v_device_cell_begin, v_device_index_map,
                     v_device_cell_ns, v_min_cell_dim, v_last_min_cell_dim, v_max_cell_dim,
                     v_last_max_cell_dim, l, max_d);
